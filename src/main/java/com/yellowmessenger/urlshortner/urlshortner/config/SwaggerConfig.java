4264a3bcf82de0bc5fc4688ddf213d1e41dd5822
karthik <karthikjoshi95@gmail.com>
2020-04-18 17:50:36 +0530

    feature commit

--- src/main/java/com/yellowmessenger/urlshortner/urlshortner/config/SwaggerConfig.java
+++ src/main/java/com/yellowmessenger/urlshortner/urlshortner/config/SwaggerConfig.java
@@ -1,29 +1,13 @@
 package com.yellowmessenger.urlshortner.urlshortner.config;
 
-import com.yellowmessenger.urlshortner.urlshortner.commons.GeneralConstants;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Component
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
-   /* @Bean
-    public Docket productApi()
-    {
-        System.out.println("added a line");
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage(GeneralConstants.WS_PACKAGE))
-                //.paths(regex("/rest.*"))
-                .build();
-    }*/
 
     public void test()
     {
